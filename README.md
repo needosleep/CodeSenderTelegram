c3462a230a0bb3a8725852daa7940ff0f186a4ba
needosleep <drobych123@gmail.com>
2024-10-15 12:35:18 +0300

    README file was added & functions were separated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-CodeSenderExtension
+# CodeSenderTelegram
+
+## Author: Drobyshevsky Evgeny Olegovich, group: M3100
+
+### Introductory
+
+
+### Possibilities
+
+
+### Get Started
+
+
+### Commands & Examples
+
+
+### History of changes
--- extension.js
+++ extension.js
@@ -4,78 +4,77 @@ const axios = require('axios');
 let chatId = '';
 const botToken = '8072757536:AAE4gyb8RMGC1fhuwwKBV_a8cMkgGXN86jQ';
 
+async function setChatId(context) {
+    while (true) {
+        const input = await vscode.window.showInputBox({ prompt: 'Enter your telegram chat_id' });
+        
+        if (input) {
+            try {
+                chatId = input;
+                const userSettings = vscode.workspace.getConfiguration('codesendertelegram');
+                await userSettings.update('chatId', chatId, vscode.ConfigurationTarget.Global);
+                vscode.window.showInformationMessage(`chat_id is updated: ${chatId}`);
+                break;
+            } catch (error) {
+                vscode.window.showErrorMessage(`error: ${error.message}`);
+                resetChatId();
+            }
+        } else {
+            vscode.window.showWarningMessage('chat_id is not set');
+            return;
+        }
+    }
+}
+
+async function sendToTelegram() {
+    const userSettings = vscode.workspace.getConfiguration('codesendertelegram');
+    chatId = userSettings.get('chatId');
+
+    if (!chatId) {
+        const input = await vscode.window.showInputBox({ prompt: 'chat_id is not set' });
+        if (input) {
+            chatId = input;
+            const userSettings = vscode.workspace.getConfiguration('codesendertelegram');
+            await userSettings.update('chatId', chatId, vscode.ConfigurationTarget.Global);
+        } else {
+            vscode.window.showWarningMessage('chat_id is not set');
+            return;
+        }
+    }
+
+    try {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const selectedText = editor.document.getText(editor.selection);
+            if (selectedText) {
+                const message = `\`\`\`\n${selectedText}\n\`\`\``;
+                await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, { chat_id: chatId, text: message, parse_mode: 'Markdown' });
+                vscode.window.showInformationMessage('Code is sent');
+            } else {
+                vscode.window.showWarningMessage('Nothing is selected');
+            }
+        }
+    } catch (error) {
+        vscode.window.showErrorMessage(`error: ${error.message}`);
+        resetChatId();
+    }
+}
+
+async function resetChatId() {
+    const userSettings = vscode.workspace.getConfiguration('codesendertelegram');
+    chatId = '';
+    await userSettings.update('chatId', chatId, vscode.ConfigurationTarget.Global);
+    vscode.window.showInformationMessage('chat_id has been reset');
+}
+
 async function activate(context) {
-    let setChatIdCommand = vscode.commands.registerCommand('extension.setChatId', async () => {
-        while (true) {
-            const input = await vscode.window.showInputBox({ prompt: 'Enter your telegram chat_id' });
-            if (input) {
-                try {
-                    await axios.post(`https://api.telegram.org/bot${botToken}/getUpdates`, { chat_id: chatId, text: "test" });
-                    chatId = input;
-                    const userSettings = vscode.workspace.getConfiguration('codesendertelegram');
-                    await userSettings.update('chatId', chatId, vscode.ConfigurationTarget.Global);
-                    vscode.window.showInformationMessage(`chat_id is updated: ${chatId}`);
-                    break;
-                } catch (error) {
-                    vscode.window.showErrorMessage(`error: ${error.message}`);
-                    chatId = '';
-                    continue;
-                }
-            } else {
-                vscode.window.showWarningMessage('chat_id is not set');
-                return;
-            }
-        }
-    });
-
-    let resetChatIdCommand = vscode.commands.registerCommand('extension.resetChatId', async() => {
-        const userSettings = vscode.workspace.getConfiguration('codesendertelegram');
-        chatId = '';
-        await userSettings.update('chatId', chatId, vscode.ConfigurationTarget.Global);
-        vscode.window.showInformationMessage('chat_id has been reset');
-    });
-
-    let sendToTelegramCommand = vscode.commands.registerCommand('extension.sendToTelegram', async () => {
-        const userSettings = vscode.workspace.getConfiguration('codesendertelegram');
-        chatId = userSettings.get('chatId');
-
-        if (!chatId) {
-            const input = await vscode.window.showInputBox({ prompt: 'chat_id is not set' });
-            if (input) {
-                try {
-                    chatId = input;
-                    await userSettings.update('chatId', chatId, vscode.ConfigurationTarget.Global);
-                } catch (error) {
-                    vscode.window.showErrorMessage(`error: ${error.message}`);
-                    chatId = '';
-                }
-            } else {
-                vscode.window.showWarningMessage('chat_id is not set');
-                return;
-            }
-        }
-
-        try {
-            const editor = vscode.window.activeTextEditor;
-            if (editor) {
-                const selectedText = editor.document.getText(editor.selection);
-                if (selectedText) {
-                    const message = `\`\`\`\n${selectedText}\n\`\`\``;
-                    await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, { chat_id: chatId, text: message, parse_mode: 'Markdown' });
-                    vscode.window.showInformationMessage('code is sent');
-                } else {
-                    vscode.window.showWarningMessage('nothing is selected');
-                }
-            }
-        } catch (error) {
-            vscode.window.showErrorMessage(`error: ${error.message}`);
-            chatId = '';
-        }
-    });
+    let setChatIdCommand = vscode.commands.registerCommand('extension.setChatId', () => setChatId(context));
+    let sendToTelegramCommand = vscode.commands.registerCommand('extension.sendToTelegram', sendToTelegram);
+    let resetChatIdCommand = vscode.commands.registerCommand('extension.resetChatId', resetChatId);
 
     context.subscriptions.push(setChatIdCommand);
-    context.subscriptions.push(resetChatIdCommand);
     context.subscriptions.push(sendToTelegramCommand);
+    context.subscriptions.push(resetChatIdCommand);
 }
 
 function deactivate() {}
